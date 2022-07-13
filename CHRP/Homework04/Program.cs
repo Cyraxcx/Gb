1a7bf35eafc4e04ebf4c32acdbb0769c20f7e27e
Kirill <cyrax9802@gmail.com>
2022-07-13 23:10:28 +0300

    work4

---
+++ CHRP/Homework04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+while ( a > 1 ) 
+{
+    if ( a % 2 == 0)
+    {
+        Console.WriteLine(a);
+    }
+    a--;
+}
