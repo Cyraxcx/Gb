b9db204c1d54c13d49af7b29bedc5a717d1c4cf0
Kirill <cyrax9802@gmail.com>
2022-07-13 22:14:43 +0300

    work1

--- CHRP/Homework01/Program.cs
+++ CHRP/Homework01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2.
+
+
+Console.WriteLine("Введите число А ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+      
+if (a > b) { max = a;
+}
+else {
+ max = b;
+}
+Console.Write("Максимальное число " );
+Console.Write( max);
