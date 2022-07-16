ecbe4d6374b6fa1b6fe2e85ec61ec30eb8fc47ee
Kirill <cyrax9802@gmail.com>
2022-07-16 19:04:17 +0300

    Seminar2homework1

--- CHRP/Seminar2Homework/work1/Program.cs
+++ CHRP/Seminar2Homework/work1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.*/
+
+Console.Write("Введите 3-х значное число:  ");
+string num = Console.ReadLine();
+Console.Write(num[1]);
