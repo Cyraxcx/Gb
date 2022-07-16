5c71cf45f03f4bd0f1ae0f9a09f2b10cff77fffa
Kirill <cyrax9802@gmail.com>
2022-07-16 20:31:39 +0300

    Seminar2homework1_2_3

---
+++ CHRP/Seminar2Homework/work2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая выводит третью цифру
+ заданного числа или сообщает, что третьей цифры нет.*/
+
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int x;
+if ( num < 99)
+{
+    Console.Write("Третьей цифры нет ");
+    
+}
+else if (num > 99 && num <= 999)
+{
+    x = num % 10;
+
+    Console.Write(x);
+}
+else if (num > 999 && num <= 9999)
+{
+    x = (num / 10) % 10;
+    Console.Write(x);
+}
+else if (num > 9999 && num <= 99999)
+{
+    x = (num / 100) % 10;
+    Console.Write(x);
+}
+else if (num > 99999 && num <= 999999)
+{
+    x = (num / 1000) % 10;
+    Console.Write(x);
+}
---
+++ CHRP/Seminar2Homework/work3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+string week (int d )
+{
+    if (d > 0 && d < 2) return "нет";   
+    else if (d > 1 && d < 3) return "нет";
+    else if (d > 2 && d < 4) return "нет";  
+    else if (d > 3 && d < 5) return "нет";
+    else if (d > 4 && d < 6) return "нет";
+    else if (d > 5 && d < 7) return "да";  
+    else if (d > 6 && d < 8) return "да";
+      
+                
+    else return "Введите число от 1 до 7 ";
+}            
+        Console.WriteLine(week(day));    
