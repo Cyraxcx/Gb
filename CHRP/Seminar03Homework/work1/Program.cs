3af118f010322bb3f875864604214d7d1f4ea6ae
Kirill <cyrax9802@gmail.com>
2022-07-20 22:09:21 +0300

    Seminar3homework1_2_3_

--- CHRP/Seminar03Homework/work1/Program.cs
+++ CHRP/Seminar03Homework/work1/Program.cs
@@ -1,29 +1,6 @@
 ﻿//Напишите программу, которая принимает на вход пятизначное число и 
 //проверяет, является ли оно палиндромом.
 
-/*Console.WriteLine("Введите 5-ти значное число");
-int num = Convert.ToInt32(Console.ReadLine());
-int reverse = 0;
-int a = 0;
-for (num > 0)
-{
-    a = num % 10;
-    reverse = (reverse* 10) + a;
-    //num = num / 10;
-}
-
-
-    if (num == reverse)
-    {
-    //reverse = (num / 10) % 10;
-//Console.WriteLine("Число является палиндромом ");
-        Console.WriteLine(reverse);
-    }
-    else 
-    {
-        Console.WriteLine("Не является палиндромом ");
-
-    }*/
    
 Console.WriteLine("Введите число: ");
 int inter = Convert.ToInt32(Console.ReadLine());
