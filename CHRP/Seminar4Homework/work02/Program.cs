2f1f544f1ceb9050f52204af2c570373be088092
Kirill <cyrax9802@gmail.com>
2022-08-08 19:18:59 +0300

    Seminar4homework1_2

---
+++ CHRP/Seminar4Homework/work02/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int sum = 0;
+while(a > 0)
+    {
+        result = a % 10;
+        sum = result + sum;
+        a = a / 10;
+        
+
+    }
+Console.WriteLine(sum);
