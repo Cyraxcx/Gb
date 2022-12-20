d28f4954bcaf56ac486fcf8ae1024a82b5cc5a5f
Kirill <cyrax9802@gmail.com>
2022-12-20 19:59:13 +0300

    OOP_w_1

---
+++ OOP/w_1/AutomateSaleHot.java
@@ -0,0 +1,69 @@
+package OOP.w_1;
+
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class AutomateSaleHot implements SaleAutomate {
+    private List<HotDrink> hotDrinks = new LinkedList<>();
+    HotDrink product;
+    
+    @Override
+    public void addProduct(HotDrink drink) {
+        this.hotDrinks.add((HotDrink) drink);
+
+    }
+
+    @Override
+    public HotDrink getProduct(String name, int volume, int tempurature) {
+        System.out.print("Введите имя продукта из списка: Americano, Cappucino, Latte \n");
+        Scanner enter3 = new Scanner(System.in);
+        String newEnter = enter3.nextLine();
+        // HotDrink product = null;
+        System.out.print("Введите объем продукта из списка: 300, 200 \n");
+        Scanner enter2 = new Scanner(System.in);
+        int newEnter2 = enter2.nextInt();
+        System.out.print("Введите температуру напитка: 70, 50 \n");
+        Scanner enter1 = new Scanner(System.in);
+        int newEnter1 = enter1.nextInt();
+
+        for (HotDrink drink : hotDrinks) {
+            if (newEnter.equals(drink.getName()) & newEnter2 == drink.getVolume()
+                    & newEnter1 == drink.getTempurature()) {
+                System.out.printf("Ваш напиток: %s его стоимость %d монет ", drink, drink.getPrice());
+                product = drink;
+                // System.out.println(product);
+                // System.out.println(product.getPrice());
+            }
+        }
+        int cash = 0;
+        System.out.print("Внесите купюру ");
+        Scanner enter4 = new Scanner(System.in);
+        Integer newEnte = enter4.nextInt();
+        cash += newEnte;
+        while (cash <= product.getPrice() || cash >= product.getPrice() ) {
+            if (cash < product.getPrice()) {
+                System.out.printf("Стоимость напитка %d. Ваш баланс %d, внесите еще: ", product.getPrice(), cash);
+                newEnte = enter4.nextInt();
+                cash += newEnte;
+            } else if (cash > product.getPrice()) {
+                cash -= product.getPrice();
+                System.out.printf("Ваш напиток %s. Ваша сдача %d ", product, cash);
+                break;
+            } else if (cash == product.getPrice()) {
+                System.out.printf("Ваш напиток %s.", product);
+                break;
+            } 
+            
+        }
+        return product;
+    }
+
+    @Override
+    public void listDrink() {
+        System.out.println(hotDrinks);
+    }
+    
+}
---
+++ OOP/w_1/SaleAutomate.java
@@ -0,0 +1,8 @@
+package OOP.w_1;
+
+public interface SaleAutomate {
+    
+    void addProduct(HotDrink drink);
+    Object getProduct(String name, int volume, int tempurature);
+    void listDrink();
+}
