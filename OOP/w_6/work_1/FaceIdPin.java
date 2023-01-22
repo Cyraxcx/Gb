8fec4b9ba676c9d6fe72436a967cbfb6ffd4bd3f
Kirill <cyrax9802@gmail.com>
2023-01-22 21:47:29 +0300

    OOP_w_6

---
+++ OOP/w_6/work_1/FaceIdPin.java
@@ -0,0 +1,8 @@
+package HW5.Task1;
+
+public class FaceIdPin extends Unlocker {
+    public static String faceIdPin(String face) {
+        System.out.println("Лицо распознано, телефон разблокирован");
+        return face;
+    }
+}
---
+++ OOP/w_6/work_1/WithoutPin.java
@@ -0,0 +1,10 @@
+package HW5.Task1;
+
+public class WithoutPin extends Unlocker{
+    public static void withoutPin() {
+        System.out.println("Телефон разблокирован");
+
+    };
+
+
+}
